66cad52b114d23c200a1bebb240e0355d633a52d
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-07-27 10:54:00 +0300

    feat: Add Role in entity

---
+++ src/main/java/com/example/backend/entity/Role.java
@@ -0,0 +1,21 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(name = "role_name", nullable = false, length = 50, unique = true)
+    private String roleName; // Örnek: "ADMIN", "USER", "SELLER"
+}
