6f6637a941387d7c640eb8c094e66bd276a695a9
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-07-27 10:49:58 +0300

    feat: Add Category in entity

---
+++ src/main/java/com/example/backend/entity/Category.java
@@ -0,0 +1,21 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(name = "category_name", nullable = false, length = 255)
+    private String name;
+}
