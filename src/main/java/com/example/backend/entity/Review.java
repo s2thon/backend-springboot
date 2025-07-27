129d95a57e27aafcca6edcb3c83bb7be73fc5e17
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-07-27 10:53:36 +0300

    feat: Add Review in entity

---
+++ src/main/java/com/example/backend/entity/Review.java
@@ -0,0 +1,39 @@
+package com.example.backend.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "review")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(length = 1000)
+    private String comment;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
