f01c72c2d147a38288b0fc9a874c817304fab908
Altuğ İkiz <20220808043@ogr.akdeniz.edu.tr>
2025-07-27 10:44:56 +0300

    feat: Add RoleDto in dto

---
+++ src/main/java/com/example/backend/dto/RoleDto.java
@@ -0,0 +1,14 @@
+// src/main/java/com/example/backend/dto/RoleDto.java
+package com.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDto {
+    private Long id;
+    private String roleName;
+}
